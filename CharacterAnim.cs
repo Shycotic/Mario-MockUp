96cd16d2e10cbfe8a273b01da700221c42c0d17a
Shycotic <42653218+Shycotic@users.noreply.github.com>
2018-10-22 17:58:27 -0400

    Add files via upload

---
+++ CharacterAnim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CharacterAnim : MonoBehaviour {
+
+    private Animator anim;
+
+   void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.SetTrigger("jump");
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            anim.SetTrigger("crouch");
+        }
+    }
+
+}
